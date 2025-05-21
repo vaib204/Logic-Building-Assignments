88b355b50a10bcf0c279b22d9f2201b527cc1e81
vaib204 <vaibhavkulkarni614@gmail.com>
2025-05-22 00:29:47 +0530

    Add files via upload

---
+++ Assignment_11/AS11-2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+void Pattern(int iNo)
+{
+  int iCnt = 0;
+
+  for (iCnt = iNo; iCnt >= 1; iCnt--)
+  {
+    printf("%d\t#\t", iCnt);
+  }
+}
+
+int main()
+{
+  int iValue = 0;
+
+  printf("Enter Number of elements:");
+  scanf("%d", &iValue);
+
+  Pattern(iValue);
+}
---
+++ Assignment_11/AS11-5.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+void Pattern(int iNo)
+{
+  int iCnt = 0;
+
+  for (iCnt = 1; iCnt <= iNo; iCnt++)
+  {
+    printf("%d\t", iCnt * 2);
+  }
+}
+
+int main()
+{
+  int iValue = 0;
+
+  printf("Enter Number of elements:");
+  scanf("%d", &iValue);
+
+  Pattern(iValue);
+}
