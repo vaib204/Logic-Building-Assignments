51c06b2aef05adedf4625deca8a71fb4d1406113
vaib204 <vaibhavkulkarni614@gmail.com>
2025-05-22 00:31:29 +0530

    Add files via upload

---
+++ Assignment_12/AS12-5.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+void Pattern(int iRow, int iCol)
+{
+  int i = 0, j = 0;
+  for (i = 1; i <= iRow; i++)
+  {
+    for (j = 1; j <= iCol; j++)
+    {
+      printf("%d\t", i);
+    }
+    printf("\n");
+  }
+}
+
+int main()
+{
+  int iValue1 = 0, iValue2;
+
+  printf("Enter the row:");
+  scanf("%d", &iValue1);
+
+  printf("Enter the column:");
+  scanf("%d", &iValue2);
+
+  Pattern(iValue1, iValue2);
+}
