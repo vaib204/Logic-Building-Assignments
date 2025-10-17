9d2f94ccebbed59b21d3bd8ca0b93614cc203adc
vaibhav kulkarni <vaibhavkulkarni614@gmail.com>
2025-10-17 18:43:52 +0530

    Add files via upload

---
+++ Assignment_47/program473.java
@@ -0,0 +1,46 @@
+
+import java.util.*;
+class Matrix
+{
+  private int Arr[][];
+
+  public Matrix(int A,int B)
+  {
+    Arr = new int[A][B];
+  }
+
+  public void  Pattern()
+  {
+    int i = 0,j = 0;
+    int icount = 0;
+
+    for(i = 0;i<Arr.length;i++)
+    {
+      for(j = Arr[i].length;j >= 1;j--)
+      {
+         System.out.print(j+"\t");
+      }
+      System.out.println();
+    }
+    
+  }
+}
+class program473
+{
+  public static void main(String a[])
+  {
+    Scanner sobj = new Scanner(System.in);
+    int irows = 0,icol = 0;
+    
+    System.out.println("Enter Rows:");
+    irows = sobj.nextInt();
+
+    System.out.println("Enter Col:");
+    icol = sobj.nextInt();
+
+    Matrix mobj  = new Matrix(irows, icol);
+
+    mobj.Pattern();
+
+  }
+}
---
+++ Assignment_47/program474.java
@@ -0,0 +1,53 @@
+
+import java.util.*;
+class Matrix
+{
+  private int Arr[][];
+
+  public Matrix(int A,int B)
+  {
+    Arr = new int[A][B];
+  }
+
+  public void  Pattern()
+  {
+    int i = 0,j = 0;
+    int icount = 0;
+
+    for(i = 0;i<Arr.length;i++)
+    {
+      for(j = 1 ; j <= Arr[i].length;j++)
+      {
+         if( j % 2 == 0)
+         {
+          System.out.print("#" + "\t");
+         }
+         else
+         {
+          System.out.print("*" + "\t");
+         }
+      }
+      System.out.println();
+    }
+    
+  }
+}
+class program474
+{
+  public static void main(String a[])
+  {
+    Scanner sobj = new Scanner(System.in);
+    int irows = 0,icol = 0;
+    
+    System.out.println("Enter Rows:");
+    irows = sobj.nextInt();
+
+    System.out.println("Enter Col:");
+    icol = sobj.nextInt();
+
+    Matrix mobj  = new Matrix(irows, icol);
+
+    mobj.Pattern();
+
+  }
+}
